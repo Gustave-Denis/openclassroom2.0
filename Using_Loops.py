25f6a06846f30f21851189151c904a36ab3c8f07
Gustave-Denis <gustave.denis@heuritech.com>
2022-06-27 11:57:57 +0200

    Add Using Loops

---
+++ Using_Loops.py
@@ -0,0 +1,12 @@
+colors = ['Light Blue', 'Hot Pink', 'Dark Green']
+for x in colors:
+    print(x)
+
+start_number = 0
+end_number = 9
+
+
+while start_number != end_number:
+    print(start_number)
+    start_number += 1
+    break
