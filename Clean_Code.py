1d565b40501327e36251ea35e480d9b4b07e8575
Gustave-Denis <gustave.denis@heuritech.com>
2022-06-27 12:14:39 +0200

    Add Clean Code

---
+++ Clean_Code.py
@@ -0,0 +1,16 @@
+print('Hello, world!')
+
+if True:
+    print('Hello, world!')
+
+if True:
+    print('Hello, world!')
+
+
+animal = 'Lion'
+if animal == 'Lion':
+    print('Meat')
+elif animal == 'Zebra':
+    print('Grass')
+else:
+    print('Water')
