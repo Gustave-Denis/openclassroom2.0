82cbe3363677b4c55e33787851c8b74562f18b33
Gustave-Denis <gustave.denis@heuritech.com>
2022-06-27 12:04:27 +0200

    Add Using Functions

---
+++ Using_Functions.py
@@ -0,0 +1,14 @@
+def adds_numbers(a, b):
+    add = a + b
+    return add
+
+
+adds_numbers(15, 32)
+
+
+def animal_food(animal):
+    if animal == 'Lion':
+        return 'Meat'
+    elif animal == 'Zebra':
+        return 'Grass'
+    return 'Nothing'
