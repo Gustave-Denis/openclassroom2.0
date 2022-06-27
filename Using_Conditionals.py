1941b2a763913b4a72447b737461fd83225ba9b3
Gustave-Denis <gustave.denis@heuritech.com>
2022-06-27 11:48:40 +0200

    Add Using Conditionals

---
+++ Using_Conditionals.py
@@ -0,0 +1,18 @@
+print(23 <=34)
+my_boolean = 50!= 70
+num1 = 130
+if num1 > 128:
+    print('Larger than 128')
+
+already_fed = False
+if already_fed:
+    print('Already fed!')
+animal = 'Warthog'
+if animal == 'Lion' or animal == 'Crocodile':
+    print('Meat')
+elif animal == 'Zebra':
+    print('Grass')
+elif animal == 'Monkey':
+    print('Fruit')
+else:
+    print('Water')
