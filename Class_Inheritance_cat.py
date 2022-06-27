00968f48a11aee02dd8721922384e944d64da1b1
Gustave-Denis <gustave.denis@heuritech.com>
2022-06-27 12:35:11 +0200

    Add Both Class Inheritance cat and main

---
+++ Class_Inheritance_cat.py
@@ -0,0 +1,12 @@
+from main import Cat
+cat = Cat(4, 18, 48)
+cat.vocalize()
+cat.print_facts()
+
+from main import Cheetah, Lion, Leopard
+cheetah = Cheetah(72,12, 120)
+lion = Lion(190, 14,80)
+leopard = Leopard (90,17,58)
+cheetah.print_facts()
+lion.print_facts()
+leopard.print_facts()
